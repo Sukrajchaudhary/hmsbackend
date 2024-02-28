87c37846062576db089822ac0a70614d69907d89
Sukrajchaudhary <sukrajchaudhary90@gmail.com>
2024-02-28 09:09:39 +0545

    lsat commit

--- Db/Db.connection.js
+++ Db/Db.connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.ConnectToDb = async () => {
     try {
-        await mongoose.connect(process.env.localURL);
+        await mongoose.connect(process.env.URL);
         console.log("Database Connected Successfully!");
     } catch (error) {
         console.error("Database connection error:", error.message);
