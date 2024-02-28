b83d0fc1ed3caecd66b66c15f3eccac8e57d2e5e
Sukrajchaudhary <sukrajchaudhary90@gmail.com>
2024-02-28 11:00:04 +0545

    deployed

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://sukrajhms.netlify.app/",
+    origin:true,
     credentials: true,
     exposedHeaders:['X-Total-Count']
   })
